3b3f7590009189c51721ce90c0379285dee25104
StephanieMarcolino <stephaniemarcolino@gmail.com>
2024-05-27 16:39:31 -0300

    suplementacao, lista de suplementacao

--- front/gestao-pecuaria-frontend/src/components/Suplementacao.vue
+++ front/gestao-pecuaria-frontend/src/components/Suplementacao.vue
@@ -2,24 +2,36 @@
   <div>
     <h2>Suplementos</h2>
     <div class="table-container">
-    <div class="button-container">
-      <button @click="resetForm()" type="button" class="btn btn-success" data-bs-toggle="modal"
-        data-bs-target="#cadastroModal" data-bs-whatever="@mdo">Adicionar Suplemento</button>
-    </div>
+      <div class="button-container">
+        <button @click="resetForm()" type="button" class="btn btn-success" data-bs-toggle="modal"
+          data-bs-target="#cadastroModal" data-bs-whatever="@mdo">Adicionar Suplemento</button>
+      </div>
       <table class="table table-bordered">
         <thead>
           <tr>
+            <th scope="col">Produto</th>
+            <th scope="col">Lote</th>
             <th scope="col">Quantidade</th>
             <th scope="col">Data Inicial</th>
             <th scope="col">Data Final</th>
+            <th scope="col">Status</th>
+            <th scope="col">Ações</th>
           </tr>
         </thead>
         <tbody>
           <tr v-for="(suplemento, index) in suplementos" :key="index">
+            <td>{{ suplemento.produto }}</td>
+            <td>{{ suplemento.lote }}</td>
             <td>{{ suplemento.quantidade }}</td>
             <td>{{ suplemento.dataInicial }}</td>
-            <td>{{ suplemento.dataFinal }}</td>
+            <td>{{ suplemento.dataFinal || '-' }}</td>
+            <td :class="{ 'status-andamento': !suplemento.dataFinal, 'status-finalizada': suplemento.dataFinal }">{{
+          suplemento.dataFinal ? 'Finalizada' : 'Em Andamento' }}</td>
             <td>
+              <button v-if="!suplemento.dataFinal" @click="finalizarSuplemento(suplemento)" class="btn-acoes btn-sm"
+                data-bs-toggle="modal" data-bs-target="#finalizarModal" data-bs-whatever="@mdo">
+                <i class="fas fa-check"></i>
+              </button>
               <button @click="editarSuplemento(suplemento)" class="btn-acoes btn-sm" data-bs-toggle="modal"
                 data-bs-target="#edicaoModal" data-bs-whatever="@mdo"><i class="fas fa-edit"></i></button>
               <button @click="confirmarExclusao(suplemento)" class="btn-acoes btn-sm" data-bs-toggle="modal"
@@ -46,18 +58,36 @@
                   placeholder="Quantidade" required>
               </div>
               <div class="mb-3 input-group">
-                <label for="dataInicial" class="input-group-text"><i class="fas fa-calendar-alt"></i>  Data Inicial</label>
-                <input v-model="formData.dataInicial" type="date" class="form-control" id="dataInicial" required>
+                <input v-model="nomeProduto" @input="filtrarProdutos" type="text" class="form-control"
+                  placeholder="Digite o nome do produto...">
+              </div>
+              <div class="list-group" v-if="nomeProduto && filteredProdutos.length">
+                <button type="button" class="list-group-item list-group-item-action" v-for="produto in filteredProdutos"
+                  :key="produto.id" @click="selecionarProduto(produto)">
+                  {{ produto.nome }}
+                </button>
+              </div>
+
+              <div class="mb-3 input-group">
+                <input v-model="lote" @input="filtrarLotes" type="text" class="form-control"
+                  placeholder="Digite o número do lote...">
+              </div>
+              <div class="list-group" v-if="lote && filteredLotes.length">
+                <button type="button" class="list-group-item list-group-item-action" v-for="lote in filteredLotes"
+                  :key="lote.id" @click="selecionarLote(lote)">
+                  {{ lote.numero }}
+                </button>
               </div>
               <div class="mb-3 input-group">
-                <label for="dataFinal" class="input-group-text"><i class="fas fa-calendar-alt"></i>  Data Final</label>
-                <input v-model="formData.dataFinal" type="date" class="form-control" id="dataFinal" required>
+                <label for="dataInicial" class="input-group-text"><i class="fas fa-calendar-alt"></i> Data
+                  Inicial</label>
+                <input v-model="formData.dataInicial" type="date" class="form-control" id="dataInicial" required>
               </div>
             </form>
           </div>
           <div class="modal-footer">
             <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
-            <button type="button" class="btn btn-primary" @click="submitForm">Enviar</button>
+            <button type="submit" class="btn btn-primary">Enviar</button>
           </div>
         </div>
       </div>
@@ -79,18 +109,35 @@
                   placeholder="Quantidade" required>
               </div>
               <div class="mb-3 input-group">
-                <span class="input-group-text"><i class="fas fa-calendar-alt"></i></span>
-                <input v-model="formData.dataInicial" type="date" class="form-control" id="dataInicialEditar" required>
+                <input v-model="nomeProduto" @input="filtrarProdutos" type="text" class="form-control"
+                  placeholder="Digite o nome do produto...">
+              </div>
+              <div class="list-group" v-if="nomeProduto && filteredProdutos.length">
+                <button type="button" class="list-group-item list-group-item-action" v-for="produto in filteredProdutos"
+                  :key="produto.id" @click="selecionarProduto(produto)">
+                  {{ produto.nome }}
+                </button>
+              </div>
+
+              <div class="mb-3 input-group">
+                <input v-model="lote" @input="filtrarLotes" type="text" class="form-control"
+                  placeholder="Digite o número do lote...">
+              </div>
+              <div class="list-group" v-if="lote && filteredLotes.length">
+                <button type="button" class="list-group-item list-group-item-action" v-for="lote in filteredLotes"
+                  :key="lote.id" @click="selecionarLote(lote)">
+                  {{ lote.numero }}
+                </button>
               </div>
               <div class="mb-3 input-group">
                 <span class="input-group-text"><i class="fas fa-calendar-alt"></i></span>
-                <input v-model="formData.dataFinal" type="date" class="form-control" id="dataFinalEditar" required>
+                <input v-model="formData.dataInicial" type="date" class="form-control" id="dataInicialEditar" required>
               </div>
             </form>
           </div>
           <div class="modal-footer">
             <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
-            <button type="button" class="btn btn-primary" @click="submitForm">Salvar</button>
+            <button type="submit" class="btn btn-primary">Salvar</button>
           </div>
         </div>
       </div>
@@ -116,6 +163,30 @@
       </div>
     </div>
 
+    <!-- Modal de Finalização de Suplemento -->
+    <div class="modal fade" id="finalizarModal" tabindex="-1" aria-labelledby="finalizarModalLabel" aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h1 class="modal-title fs-5" id="finalizarModalLabel">Finalizar Suplemento</h1>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body">
+            <form @submit.prevent="finalizarSuplementoSubmit">
+              <div class="mb-3 input-group">
+                <label for="dataFinal" class="input-group-text"><i class="fas fa-calendar-alt"></i> Data Final</label>
+                <input v-model="formData.dataFinal" type="date" class="form-control" id="dataFinal" required>
+              </div>
+            </form>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
+            <button type="submit" class="btn btn-primary">Finalizar</button>
+          </div>
+        </div>
+      </div>
+    </div>
+
   </div>
 </template>
 
@@ -127,17 +198,26 @@ export default {
   data() {
     return {
       suplementos: [],
+      produtos: [],
+      lotes: [],
+      filteredProdutos: [],
+      filteredLotes: [],
+      nomeProduto: '',
+      lote: '',
       formData: {
         id: null,
+        produto: '',
+        lote: '',
         quantidade: '',
         dataInicial: '',
-        dataFinal: '',
       },
       modalTitle: 'Cadastro de Suplemento',
     }
   },
   mounted() {
     this.buscarSuplementosDaApi();
+    this.buscarProdutosDaApi();
+    this.buscarLotesDaApi();
   },
   methods: {
     async buscarSuplementosDaApi() {
@@ -148,6 +228,48 @@ export default {
         console.error('Erro ao buscar suplementos da API:', error);
       }
     },
+    async buscarProdutosDaApi() {
+      try {
+        const response = await api.get('http://127.0.0.1:8000/produtos/');
+        this.produtos = response.data;
+      } catch (error) {
+        console.error('Erro ao buscar produtos da API:', error);
+      }
+    },
+    async buscarLotesDaApi() {
+      try {
+        const response = await api.get('http://127.0.0.1:8000/lotes/');
+        this.lotes = response.data;
+      } catch (error) {
+        console.error('Erro ao buscar lotes da API:', error);
+      }
+    },
+    filtrarProdutos(entrada) {
+      if (!entrada) {
+        this.filteredProdutos = this.produtos;
+        return;
+      }
+      this.filteredProdutos = this.produtos.filter(produto => produto.nome.toLowerCase().includes(this.nomeProduto));
+    },
+    filtrarLotes(entrada) {
+      if (!entrada) {
+        this.filteredLotes = this.lotes;
+        return;
+      }
+      this.filteredLotes = this.lotes.filter(lote => lote.numero.toLowerCase().includes(this.lote));
+    },
+    selecionarProduto(produto) {
+      this.formData.produto = produto.id;
+      this.nomeProduto = produto.nome;
+      this.filteredProdutos = [];
+    },
+
+    selecionarLote(lote) {
+      this.formData.lote = lote.id;
+      this.lote = lote.numero;
+      this.filteredLotes = [];
+    },
+
     editarSuplemento(suplemento) {
       this.modalTitle = 'Editar Suplemento';
       this.formData = {
@@ -233,6 +355,14 @@ export default {
         console.error('Botão de fechar não encontrado no modal:', modalId);
       }
     },
+    finalizarSuplemento(suplemento) {
+      this.formData = {
+        id: suplemento.id,
+        quantidade: suplemento.quantidade,
+        dataInicial: suplemento.dataInicial,
+      };
+      this.fecharModal
+    },
   }
 };
 </script>
@@ -248,18 +378,20 @@ export default {
 }
 
 .button-container {
-  text-align: left; 
-  margin-bottom: 20px; 
+  text-align: left;
+  margin-bottom: 20px;
 }
 
 .table-container table thead tr th {
-  border-bottom: 2px solid #4CAF50; /* Adiciona uma borda verde na parte inferior */
+  border-bottom: 2px solid #4CAF50;
+  /* Adiciona uma borda verde na parte inferior */
 }
 
 .btn-acoes {
   background-color: transparent;
   border: none;
   padding: 0;
+  margin: 5px
 }
 
 .btn-acoes i {
@@ -268,7 +400,16 @@ export default {
 
 .button-group {
   display: flex;
-  gap: 10px; 
+  gap: 10px;
 }
 
+.status-andamento {
+  color: #ff9800;
+  /* Laranja */
+}
+
+.status-finalizada {
+  color: #4caf50;
+  /* Verde */
+}
 </style>

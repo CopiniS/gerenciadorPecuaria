87cec6b50b0b750ed50acc796cebb883d3bbd39a
StephanieMarcolino <stephaniemarcolino@gmail.com>
2024-06-17 19:57:55 -0300

    ajustando menu

--- front/gestao-pecuaria-frontend/src/components/NavbarComponent.vue
+++ front/gestao-pecuaria-frontend/src/components/NavbarComponent.vue
@@ -2,14 +2,17 @@
   <div>
     <nav class="navbar navbar-dark bg-dark fixed-top custom-navbar">
       <div class="container-fluid">
-        <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar" aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar"
+          aria-controls="offcanvasDarkNavbar" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <a class="navbar-brand" href="#">Nome da Aplicação</a>
         <a class="navbar-brand" href="#">Propriedade atual: {{ nomeProp }}</a>
-        <div class="offcanvas offcanvas-start text-bg-dark custom-offcanvas justify-content-center" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
+        <div class="offcanvas offcanvas-start text-bg-dark custom-offcanvas justify-content-center" tabindex="-1"
+          id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
           <div class="offcanvas-header">
-            <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+            <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas"
+              aria-label="Close"></button>
           </div>
           <div class="offcanvas-body">
             <ul class="navbar-nav justify-content-center flex-grow-1 pe-3">
@@ -22,34 +25,62 @@
               </li>
               <hr class="custom-hr">
               <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                  aria-expanded="false">
                   <i class="fas fa-landmark"></i> Propriedade
                 </a>
                 <ul class="dropdown-menu justify-content-center">
-                  <li><a class="dropdown-item" href="/propriedade"><i class="fas fa-edit"></i> Editar Propriedade</a></li>
+                  <li><a class="dropdown-item" href="/propriedade"><i class="fas fa-edit"></i> Editar Propriedade</a>
+                  </li>
                   <hr class="custom-hr">
-                  <li><a class="dropdown-item" href="/propriedades"><i class="fas fa-exchange-alt"></i> Trocar de Propriedade</a></li>
+                  <li><a class="dropdown-item" href="/propriedades"><i class="fas fa-exchange-alt"></i> Trocar de
+                      Propriedade</a></li>
                   <hr class="custom-hr">
-                  <li><a class="dropdown-item" href="/cadastropropriedade"><i class="fas fa-plus"></i> Cadastrar Propriedade</a></li>
+                  <li><a class="dropdown-item" href="/cadastropropriedade"><i class="fas fa-plus"></i> Cadastrar
+                      Propriedade</a></li>
                   <hr class="custom-hr">
-                  <li><a class="dropdown-item" href="/propriedade" @click.prevent="confirmarApagarPropriedade"><i class="fas fa-trash-alt"></i> Excluir Propriedade</a></li>
+                  <li><a class="dropdown-item" href="/propriedade" @click.prevent="confirmarApagarPropriedade"><i
+                        class="fas fa-trash-alt"></i> Excluir Propriedade</a></li>
                 </ul>
               </li>
               <hr class="custom-hr">
-              <li class="nav-item">
-                <a href="/compraprodutos" class="nav-link"><i class="fas fa-box"></i> Compra de produtos</a>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                  aria-expanded="false">
+                  <i class="fas fa-box"></i> Produto
+                </a>
+                <ul class="dropdown-menu justify-content-center">
+                  <li><a class="dropdown-item" href="/produtos"><i class="fas fa-list"></i> Lista de Produtos</a></li>
+                  <hr class="custom-hr">
+                  <li><a class="dropdown-item" href="/compraprodutos"><i class="fas fa-shopping-cart"></i> Compra de
+                      Produtos</a></li>
+                  <hr class="custom-hr">
+                  <li><a class="dropdown-item" href="/aplicacoes-produtos"><i class="fas fa-syringe"></i> Aplicações de
+                      Produtos</a></li>
+                </ul>
               </li>
+
               <hr class="custom-hr">
-              <li class="nav-item">
-                <a href="/produtos" class="nav-link"><i class="fas fa-box"></i> Produtos</a>
-              </li>
-              <hr class="custom-hr">
-              <li class="nav-item">
-                <a href="/animais" class="nav-link"><i class="fas fa-paw"></i> Animais</a>
-              </li>
-              <hr class="custom-hr">
-              <li class="nav-item">
-                <a href="/raca" class="nav-link"><i class="fas fa-dna"></i> Raças</a>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                  aria-expanded="false">
+                  <i class="fas fa-paw"></i> Animais
+                </a>
+                <ul class="dropdown-menu justify-content-center">
+                  <li><a class="dropdown-item" href="/animais"><i class="fas fa-paw"></i> Lista de Animais</a></li>
+                  <hr class="custom-hr">
+                  <li><a class="dropdown-item" href="/raca"><i class="fas fa-dna"></i> Raças</a></li>
+                  <hr class="custom-hr">
+                  <li><a class="dropdown-item" href="/Pesagens"><i class="fas fa-weight"></i> Pesagens</a></li>
+                  <hr class="custom-hr">
+                  <li><a class="dropdown-item" href="/vendas-animais"><i class="fas fa-calculator"></i> Vendas de
+                      animais</a></li>
+                  <hr class="custom-hr">
+                  <li><a class="dropdown-item" href="/inseminacoes"><i class="fas fa-capsules"></i> Inseminações</a>
+                  </li>
+                  <hr class="custom-hr">
+                  <li><a class="dropdown-item" href="/Movimentacoes"><i class="fas fa-cogs"></i> Movimentacoes</a></li>
+                </ul>
               </li>
               <hr class="custom-hr">
               <li class="nav-item">
@@ -61,32 +92,14 @@
               </li>
               <hr class="custom-hr">
               <li class="nav-item">
-                <a href="/Pesagens" class="nav-link"><i class="fas fa-weight"></i> Pesagens</a>
-              </li>
-              <hr class="custom-hr">
-              <li class="nav-item">
-                <a href="/Suplementacao" class="nav-link"><i class="fas fa-prescription-bottle-alt"></i> Suplementações</a>
-              </li>
-              <hr class="custom-hr">
-              <li class="nav-item">
-                <a href="/aplicacoes-produtos" class="nav-link"><i class="fas fa-syringe"></i> Aplicações de Produtos</a>
-              </li>
-              <hr class="custom-hr">
-              <li class="nav-item">
-                <a href="/vendas-animais" class="nav-link"><i class="fas fa-calculator"></i> Vendas de animais</a>
-              </li>
-              <hr class="custom-hr">
-              <li class="nav-item">
-                <a href="/inseminacoes" class="nav-link"><i class="fas fa-capsules"></i> Inseminações</a>
+                <a href="/Suplementacao" class="nav-link"><i class="fas fa-prescription-bottle-alt"></i>
+                  Suplementações</a>
               </li>
               <hr class="custom-hr">
               <li class="nav-item">
                 <a href="/outras-despesas" class="nav-link"><i class="fas fa-money-bill-wave"></i> Outras Despesas</a>
               </li>
-              <hr class="custom-hr">
-              <li class="nav-item">
-                <a href="/Movimentacoes" class="nav-link"><i class="fas fa-cogs"></i>Movimentacoes</a>
-              </li>
+
               <hr class="custom-hr">
               <li class="nav-item">
                 <a href="/login" class="nav-link"><i class="fas fa-sign-out-alt"></i> Sair</a>
@@ -178,9 +191,12 @@ li {
 }
 
 a {
-  color: #cfcfcf !important; /* Tom de cinza claro para o texto */
-  white-space: nowrap; /* Impede a quebra de linha */
-  font-size: 1.1em; /* Aumenta o tamanho da letra */
+  color: #cfcfcf !important;
+  /* Tom de cinza claro para o texto */
+  white-space: nowrap;
+  /* Impede a quebra de linha */
+  font-size: 1.1em;
+  /* Aumenta o tamanho da letra */
 }
 
 
@@ -192,59 +208,77 @@ a {
 }
 
 .nav-item .dropdown-item i {
-  margin-right: 8px; /* Espaço entre o ícone e o texto */
+  margin-right: 8px;
+  /* Espaço entre o ícone e o texto */
 }
 
 .dropdown-menu {
-  background-color: #252525 !important; /* Fundo cinza escuro para o dropdown */
-  border: none; /* Remove a borda do dropdown */
+  background-color: #252525 !important;
+  /* Fundo cinza escuro para o dropdown */
+  border: none;
+  /* Remove a borda do dropdown */
 }
 
 .dropdown-item:hover {
-  background-color: #3a3a3a !important; /* Fundo cinza mais claro no hover */
-  color: #cfcfcf !important; /* Cor do texto no hover */
+  background-color: #3a3a3a !important;
+  /* Fundo cinza mais claro no hover */
+  color: #cfcfcf !important;
+  /* Cor do texto no hover */
 }
 
 .nav-item:hover {
-  background-color: #3a3a3a !important; /* Fundo cinza mais claro no hover */
-  color: #cfcfcf !important; /* Cor do texto no hover */
+  background-color: #3a3a3a !important;
+  /* Fundo cinza mais claro no hover */
+  color: #cfcfcf !important;
+  /* Cor do texto no hover */
 }
 
 .custom-offcanvas {
-  width: 350px !important; /* Largura personalizada para o menu lateral */
+  width: 350px !important;
+  /* Largura personalizada para o menu lateral */
 }
 
 .custom-hr {
-  border-color: black; /* Cor preta para as linhas hr */
+  border-color: black;
+  /* Cor preta para as linhas hr */
 }
 
 .custom-close {
-  color: black; /* Cor preta para o botão de fechar */
-  opacity: 1; /* Garantir que o botão esteja totalmente opaco */
+  color: black;
+  /* Cor preta para o botão de fechar */
+  opacity: 1;
+  /* Garantir que o botão esteja totalmente opaco */
 }
 
 
 /* Personaliza a barra de rolagem */
 ::-webkit-scrollbar {
-  width: 8px; /* Largura da barra de rolagem */
-  height: 8px; /* Altura da barra de rolagem horizontal */
+  width: 8px;
+  /* Largura da barra de rolagem */
+  height: 8px;
+  /* Altura da barra de rolagem horizontal */
 }
 
 /* Fundo da barra de rolagem */
 ::-webkit-scrollbar-track {
-  background: #252525; /* Cor do fundo da barra de rolagem */
+  background: #252525;
+  /* Cor do fundo da barra de rolagem */
 }
 
 /* Cor da barra de rolagem */
 ::-webkit-scrollbar-thumb {
-  background-color: #101010; /* Cor da barra de rolagem */
-  border-radius: 10px; /* Bordas arredondadas */
-  border: 2px solid #252525; /* Adiciona uma borda */
+  background-color: #101010;
+  /* Cor da barra de rolagem */
+  border-radius: 10px;
+  /* Bordas arredondadas */
+  border: 2px solid #252525;
+  /* Adiciona uma borda */
 }
 
 /* Barra de rolagem ao passar o mouse */
 ::-webkit-scrollbar-thumb:hover {
-  background: #575656; /* Cor da barra de rolagem ao passar o mouse */
+  background: #575656;
+  /* Cor da barra de rolagem ao passar o mouse */
 }
 
 /* Adiciona margem superior ao conteúdo para evitar sobreposição com o navbar */
@@ -252,5 +286,3 @@ a {
   margin-top: 50px;
 }
 </style>
-
-

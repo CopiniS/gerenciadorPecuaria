8eab2473d92bcd69ea580a6049a77e7bf617b2e1
Copini <gabrielcopini12@gmail.com>
2024-07-21 12:03:57 -0300

    criado um arquivo mixin para fazer os validadores necessários

---
+++ front/gestao-pecuaria-frontend/src/mixins/validadores.js
@@ -0,0 +1,11 @@
+export const validadoresMixin = {
+    methods: {
+        // dataValidador(string){
+        //     let valido = false;
+        //     if(/^\d{4}-\d{2}-\d{2}$/.test(string)){
+        //         valido = true;
+        //     }
+        //     return valido;
+        // },
+    }
+}

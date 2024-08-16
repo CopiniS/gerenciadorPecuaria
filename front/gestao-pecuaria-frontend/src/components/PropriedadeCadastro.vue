cb571985626bfa6c60b8ccb6b085c9f772e5bb34
Copini <gabrielcopini12@gmail.com>
2024-08-16 15:03:48 -0300

    Select do estado com problemas

--- front/gestao-pecuaria-frontend/src/components/PropriedadeCadastro.vue
+++ front/gestao-pecuaria-frontend/src/components/PropriedadeCadastro.vue
@@ -28,14 +28,22 @@
                     <input v-model="formData.endereco" :class="{'is-invalid': !isEnderecoValido}" type="text" class="form-control"
                         :placeholder="enderecoPlaceholder" id="endereco" title="Endereço da Propriedade">
                 </div>
-                <div class="mb-3 input-group">
-                    <span class="input-group-text" title="Estado da Propriedade"><i class="fas fa-flag"></i></span>
-                    <select v-model="formData.estado" :class="{'is-invalid': !isEstadoValido}" class="form-select"
-                        @change="buscarCidadesPorEstado($event.target.value)" title="Estado da Propriedade">
-                        <option value="" disabled>{{ estadoPlaceholder }}</option>
-                        <option v-for="estado in estados" :key="estado.id" :value="estado.nome">{{ estado.nome
-                            }}</option>
-                    </select>
+                <div ref="dropdown" class="select mb-3 input-group" @keydown.up.prevent="navigateOptionsEstado('up')"
+                  @keydown.down.prevent="navigateOptionsEstado('down')" @keydown.enter.prevent="selectHighlightedEstado">
+                  <div class="select-option mb-3 input-group" @click.stop="toggleDropdownEstado">
+                    <span class="input-group-text" title="Estado da Propriedade"><i class="fas fa-user-tag"></i></span>
+                    <input v-model="formData.estado" :class="{ 'is-invalid': !isEstadoValido }" @input="inputEstado"
+                      @click="filterEstados" @keydown.up.prevent="navigateOptionsEstado('up')"
+                      @keydown.down.prevent="navigateOptionsEstado('down')" type="text" class="form-control"
+                      :placeholder="estadoPlaceholder" id="caixa-select" title="Estado da Propriedade">
+                  </div>
+                  <div class="itens" v-show="dropdownEstadoOpen">
+                    <ul class="options">
+                      <li v-for="(estado, index) in estadosFiltrados" :key="estado.id" :value="estado.nome"
+                        @click="selectEstado(estado)" :class="{ 'highlighted': index === highlightedIndexEstado }">{{
+                        estado.nome }}</li>
+                    </ul>
+                  </div>
                 </div>
                 <div class="mb-3 input-group">
                     <span class="input-group-text"><i class="fas fa-city"></i></span>
@@ -83,6 +91,9 @@ export default {
     return {
         activeTab: 'cadastro',  // Aba inicial é 'cadastro'
         estados: [],
+        estadosFiltrados: [],
+        dropdownEstadoOpen: false,
+        highlightedIndexEstado: -1,
         cidades: [],
         formData: {
             id: null,
@@ -186,6 +197,74 @@ export default {
             alert('Erro ao enviar requisição. Verifique o console para mais detalhes');
         }
         this.$router.push('/propriedades');
+      }
+    },
+
+
+//LÓGICA DOS SELECTS----------------------------------------------------------------------------------------------------------------------------------------------------
+  filterEstados() {
+        this.estadosFiltrados = this.estados.filter(estado => estado.nome.toLowerCase().includes(this.formData.estado.toLowerCase()));
+    },
+
+    selectEstado(estado) {
+        this.formData.estado = estado.nome;
+        this.estadosFiltrados = [];
+        this.dropdownEstadoOpen = false;
+    },
+
+    toggleDropdownEstado() {
+      this.dropdownEstadoOpen = !this.dropdownEstadoOpen;
+      let nomeCorreto = false;
+
+      if(!this.dropdownEstadoOpen){
+        this.estadosFiltrados.forEach(estado => {
+          if(estado.nome === this.formData.estado){
+            this.formData.estado = estado.nome;
+            this.estadosFiltrados = [];
+            nomeCorreto = true;
+          }
+        });
+        if(!nomeCorreto){
+          this.formData.estado = '';
+        }
+      }
+    },
+
+    handleClickOutsideEstado(event) {
+      if (this.dropdownEstadoOpen && this.$refs.dropdown && !this.$refs.dropdown.contains(event.target)) {
+        this.dropdownEstadoOpen = false;
+      }
+      let nomeCorreto = false;
+      if(!this.dropdownEstadoOpen){
+        this.estados.forEach(estado => {
+          if(estado.nome === this.formData.estado){
+            this.formData.estado = estado.nome;
+            this.estadosFiltrados = [];
+            nomeCorreto = true;
+          }
+        });
+        if(!nomeCorreto){
+          this.formData.estado = '';
+        }
+      }
+    },
+
+    inputEstado(){
+      this.filterEstados();
+      this.dropdownEstadoOpen = true;
+    },
+
+    navigateOptionsEstado(direction) {
+      if (direction === 'up' && this.highlightedIndexEstado > 0) {
+        this.highlightedIndexEstado--;
+      } else if (direction === 'down' && this.highlightedIndexEstado < this.estadosFiltrados.length - 1) {
+        this.highlightedIndexEstado++;
+      }
+    },
+
+    selectHighlightedEstado() {
+      if (this.highlightedIndexEstado >= 0 && this.highlightedIndexEstado < this.estadosFiltrados.length) {
+        this.selectEstado(this.estadosFiltrados[this.highlightedIndexEstado]);
       }
     },
 
@@ -413,5 +492,41 @@ export default {
 
 #legenda {
     font-size: 16px;
+}
+
+.select-option {
+  width: 100%;
+  cursor: pointer;
+}
+
+.itens {
+  position: absolute;
+  background-color: #fff;
+  color: #000;
+  border: 1px solid #ccc;
+  border-radius: 7px;
+  width: 100%;
+  margin-top: 40px;
+  z-index: 999;
+  padding: 20px;
+}
+
+.options {
+  max-height: 200px;
+  /* Ajuste a altura conforme necessário */
+  overflow-y: auto;
+  border: 1px solid #ddd;
+  margin: 0;
+  padding: 0;
+  list-style: none;
+}
+
+.options li {
+  padding: 10px;
+  cursor: pointer;
+}
+
+.options li:hover {
+  background-color: #f0f0f0;
 }
 </style>

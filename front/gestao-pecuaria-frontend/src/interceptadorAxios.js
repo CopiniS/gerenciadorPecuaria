e3974d69fe23b0202a8f404d302412ebb8d2b4e0
Copini <gabrielcopini12@gmail.com>
2024-08-28 11:17:24 -0300

    Ajuste nos erros dos tokens, falta testes ainda

--- front/gestao-pecuaria-frontend/src/interceptadorAxios.js
+++ front/gestao-pecuaria-frontend/src/interceptadorAxios.js
@@ -1,42 +1,52 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:8000/',
+  baseURL: 'http://127.0.0.1:8000/', 
 });
 
-// Função para criar e retornar um novo interceptor
-const createInterceptor = () => {
-  return api.interceptors.request.use(
-    async (config) => {
-      const accessToken = localStorage.getItem('access_token');
+api.interceptors.request.use(
+  config => {
+    const token = localStorage.getItem('access_token');    
+    if (token) {
+      config.headers['Authorization'] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
-      if (accessToken) {
-        const expirationTime = localStorage.getItem('access_exp') * 1000;
-        const currentTime = new Date().getTime();
-
-        if (expirationTime < currentTime || expirationTime === null) {
-          await renovarToken();
-          config.headers.Authorization = `Bearer ${localStorage.getItem('access_token')}`;
-        } else {
-          config.headers.Authorization = `Bearer ${accessToken}`;
+api.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  async (error) => {
+    const originalRequest = error.config;
+    if (error.response.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+      
+      const refreshToken = localStorage.getItem('refresh_token');
+      if (refreshToken) {
+        try {
+          const { data } = await api.post('/token/refresh/', { token: refreshToken });
+          
+          localStorage.setItem('access_token', data.access_token);
+          api.defaults.headers.common['Authorization'] = `Bearer ${data.access_token}`;
+          
+          return api(originalRequest);
+        } catch (err) {
+          // Lidar com erro ao obter novo token
+          localStorage.removeItem('access_token');
+          localStorage.removeItem('refresh_token');
+          alert('Sua sessão foi expirada');
+          localStorage.clear();
+          this.$router.push('/login');
         }
       }
-      return config;
-    },
-    (error) => {
-      return Promise.reject(error);
     }
-  );
-};
-
-async function renovarToken() {
-  const refreshToken = localStorage.getItem('refresh_token')
-  const response = await axios.post('http://127.0.0.1:8000/token/refresh/', { refresh: refreshToken });
-  localStorage.setItem('access_token', response.data.access)
-  localStorage.setItem('access_exp', response.data.expires)
-}
-
-// Crie o interceptor ao carregar o módulo
-createInterceptor();
+    return Promise.reject(error);
+  }
+);
 
 export default api;

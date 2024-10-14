c0a0da3e6b8f346c2060ad04f42722595309036e
StephanieMarcolino <stephaniemarcolino@gmail.com>
2024-10-14 17:30:57 -0300

    componente data

--- front/gestao-pecuaria-frontend/src/views/TelaPropriedade.vue
+++ front/gestao-pecuaria-frontend/src/views/TelaPropriedade.vue
@@ -2,28 +2,15 @@
   <div class="background">
     <nav>
       <div class="nav nav-tabs" id="nav-tab" role="tablist">
-        <button
-          class="nav-link active"
-          id="nav-vet-tab"
-          data-bs-toggle="tab"
-          data-bs-target="#nav-vet"
-          type="button"
-          role="tab"
-          aria-controls="nav-vet"
-          aria-selected="true"
-        >
+        <button class="nav-link active" id="nav-vet-tab" data-bs-toggle="tab" data-bs-target="#nav-vet" type="button"
+          role="tab" aria-controls="nav-vet" aria-selected="true">
           Lista de Propriedades
         </button>
       </div>
     </nav>
     <div class="tab-content" id="nav-tabContent">
-      <div
-        class="tab-pane fade show active"
-        id="nav-home"
-        role="tabpanel"
-        aria-labelledby="nav-vet-tab"
-        tabindex="0"
-      ></div>
+      <div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-vet-tab" tabindex="0">
+      </div>
     </div>
 
     <h2>Lista de Propriedades</h2>
@@ -39,38 +26,19 @@
             <i class="fas fa-chevron-down"></i>
           </button>
         </div>
-        <form
-          @submit.prevent="aplicarFiltro"
-          @keyup.enter="aplicarFiltro"
-          class="row g-3 align-items-center"
-          v-show="mostrarFormulario"
-        >
+        <form @submit.prevent="aplicarFiltro" @keyup.enter="aplicarFiltro" class="row g-3 align-items-center"
+          v-show="mostrarFormulario">
           <div class="col-auto d-flex align-items-center">
             <label for="nome" class="form-label me-2">Nome</label>
-            <input
-              type="text"
-              class="form-control input-consistente"
-              id="nome"
-              v-model="filtro.nome"
-            />
+            <input type="text" class="form-control input-consistente" id="nome" v-model="filtro.nome" />
           </div>
           <div class="col-auto d-flex align-items-center">
             <label for="cidade" class="form-label me-2">Cidade</label>
-            <input
-              type="text"
-              class="form-control input-consistente"
-              id="cidade"
-              v-model="filtro.cidade"
-            />
+            <input type="text" class="form-control input-consistente" id="cidade" v-model="filtro.cidade" />
           </div>
           <div class="col-auto d-flex align-items-center">
             <label for="estado" class="form-label me-2">Estado</label>
-            <input
-              type="text"
-              class="form-control input-consistente"
-              id="estado"
-              v-model="filtro.estado"
-            />
+            <input type="text" class="form-control input-consistente" id="estado" v-model="filtro.estado" />
           </div>
           <div class="col-12 d-flex justify-content-start mt-3">
             <button class="btn btn-secondary me-2" @click="limparFiltro">
@@ -84,49 +52,33 @@
       <div>
         <div class="table-container">
           <div class="button-container">
-            <button
-              @click="acessarCadastro()"
-              type="button"
-              class="btn btn-success"
-            >
+            <button @click="acessarCadastro()" type="button" class="btn btn-success">
               Cadastrar Propriedade
             </button>
-            <button
-              @click="
-                () => {
-                  this.$router.push('/piquetes');
-                }
-              "
-              type="button"
-              class="btn btn-success"
-            >
+            <button @click="() => {
+                this.$router.push('/piquetes');
+              }
+              " type="button" class="btn btn-success">
               Lista de Piquetes
             </button>
-            <RelatorioPdf
-              titulo="Relatório de Propriedades"
-              :cabecalho="['Produtor: ' + nomeProdutor]"
-              :colunas="[
-                'Nome',
-                'Endereço',
-                'Cidade',
-                'Estado',
-                'Latitude',
-                'Longitude',
-                'Área',
-              ]"
-              :dados="
-                propriedades.map((propriedade) => [
-                  propriedade.nome,
-                  propriedade.endereco,
-                  propriedade.cidade,
-                  propriedade.estado,
-                  formatarValor(replacePontoVirgula(propriedade.latitude)),
-                  formatarValor(replacePontoVirgula(propriedade.longitude)),
-                  formatarValor(replacePontoVirgula(propriedade.area)),
-                ])
-              "
-              :mostrarSoma="true"
-            />
+            <RelatorioPdf titulo="Relatório de Propriedades" :cabecalho="['Produtor: ' + nomeProdutor]" :colunas="[
+              'Nome',
+              'Endereço',
+              'Cidade',
+              'Estado',
+              'Latitude',
+              'Longitude',
+              'Área',
+            ]" :dados="propriedades.map((propriedade) => [
+                propriedade.nome,
+                propriedade.endereco,
+                propriedade.cidade,
+                propriedade.estado,
+                formatarValor(replacePontoVirgula(propriedade.latitude)),
+                formatarValor(replacePontoVirgula(propriedade.longitude)),
+                formatarValor(replacePontoVirgula(propriedade.area)),
+              ])
+                " :mostrarSoma="true" />
           </div>
           <table class="table table-bordered">
             <thead>
@@ -159,30 +111,17 @@
                   {{ formatarValor(replacePontoVirgula(propriedade.area)) }}
                 </td>
                 <td>
-                  <button
-                    v-if="propriedadeAtualId == propriedade.id"
-                    @click="acessarEdicao(propriedade)"
-                    class="btn-acoes btn-sm"
-                    title="Editar Propriedade"
-                  >
+                  <button v-if="propriedadeAtualId == propriedade.id" @click="acessarEdicao(propriedade)"
+                    class="btn-acoes btn-sm" title="Editar Propriedade">
                     <i class="fas fa-edit"></i>
                   </button>
-                  <button
-                    v-if="propriedadeAtualId == propriedade.id"
-                    @click="confirmarExclusao(propriedade)"
-                    class="btn-acoes btn-sm"
-                    data-bs-toggle="modal"
-                    title="Excluir Propriedade"
-                    data-bs-target="#confirmacaoExclusaoModal"
-                  >
+                  <button v-if="propriedadeAtualId == propriedade.id" @click="confirmarExclusao(propriedade)"
+                    class="btn-acoes btn-sm" data-bs-toggle="modal" title="Excluir Propriedade"
+                    data-bs-target="#confirmacaoExclusaoModal">
                     <i class="fas fa-trash-alt"></i>
                   </button>
-                  <button
-                    v-if="propriedadeAtualId != propriedade.id"
-                    @click="trocaPropriedade(propriedade.id)"
-                    class="btn-acoes btn-sm"
-                    title="Trocar Propriedade"
-                  >
+                  <button v-if="propriedadeAtualId != propriedade.id" @click="trocaPropriedade(propriedade.id)"
+                    class="btn-acoes btn-sm" title="Trocar Propriedade">
                     <i class="fas fa-exchange-alt"></i>
                   </button>
                 </td>
@@ -192,42 +131,24 @@
         </div>
 
         <!-- Modal de Confirmação de Exclusão -->
-        <div
-          class="modal fade"
-          id="confirmacaoExclusaoModal"
-          tabindex="-1"
-          aria-labelledby="confirmacaoExclusaoModalLabel"
-          aria-hidden="true"
-        >
+        <div class="modal fade" id="confirmacaoExclusaoModal" tabindex="-1"
+          aria-labelledby="confirmacaoExclusaoModalLabel" aria-hidden="true">
           <div class="modal-dialog modal-dialog-centered">
             <div class="modal-content">
               <div class="modal-header">
                 <h5 class="modal-title" id="confirmacaoExclusaoModalLabel">
                   Confirmação de Exclusão
                 </h5>
-                <button
-                  type="button"
-                  class="btn-close"
-                  data-bs-dismiss="modal"
-                  aria-label="Close"
-                ></button>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
               </div>
               <div class="modal-body">
                 Tem certeza de que deseja excluir esta Propriedade?
               </div>
               <div class="modal-footer">
-                <button
-                  type="button"
-                  class="btn btn-secondary"
-                  data-bs-dismiss="modal"
-                >
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">
                   Cancelar
                 </button>
-                <button
-                  type="button"
-                  class="btn btn-danger"
-                  @click="apagarPropriedade()"
-                >
+                <button type="button" class="btn btn-danger" @click="apagarPropriedade()">
                   Excluir
                 </button>
               </div>
@@ -400,13 +321,11 @@ export default {
 
 <style scoped>
 @import url("https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css");
-
 .background {
   background-color: #ededef;
   min-height: 100vh;
   padding: 20px;
   position: relative;
-  z-index: 0; /* Garante que a imagem de fundo fique na camada mais baixa */
 }
 
 .background::before {
@@ -421,19 +340,20 @@ export default {
   background-position: center;
   background-size: 40%;
   opacity: 0.1;
-  z-index: 0; /* A imagem de fundo deve estar abaixo do conteúdo */
 }
 
 nav,
 .tab-content {
   position: relative;
-  z-index: 1; /* Coloca o conteúdo acima da marca d'água */
+  z-index: 1;
+  /* Coloca o conteúdo acima da marca d'água */
 }
 
 .table-container,
 .button-container {
   position: relative;
-  z-index: 1; /* Garante que as tabelas e botões estejam acima da imagem de fundo */
+  z-index: 1;
+  /* Garante que as tabelas e botões estejam acima da imagem de fundo */
 }
 
 .table-container table tbody tr td {
@@ -457,14 +377,16 @@ nav,
 .btn-success {
   margin-right: 10px;
   margin-bottom: 10px;
-  z-index: 2; /* Garante que o botão esteja acima da imagem */
+  z-index: 2;
+  /* Garante que o botão esteja acima da imagem */
 }
 
 .btn-acoes {
   background-color: transparent;
   border: none;
   padding: 0;
-  z-index: 2; /* Garante que o botão de ação esteja acima da imagem */
+  z-index: 2;
+  /* Garante que o botão de ação esteja acima da imagem */
 }
 
 .btn-acoes i {
